d5fbfc78ce430dac50815dba8fa14131d96f7678
bubuko <iurgnaw@yahoo.com>
2018-01-20 00:50:40 +0800

    no message

--- Sources/App/Routes/Routes.swift
+++ Sources/App/Routes/Routes.swift
@@ -1,29 +1,32 @@
 import Vapor
 
 extension Droplet {
-    func setupRoutes() throws {
-        get("hello") { req in
-            var json = JSON()
-            try json.set("hello", "world")
-            return json
-        }
-
-        get("plaintext") { req in
-            return "Hello, world!"
-        }
-
-        get("/") { req in
-            return "双!"
-        }
-
-        // response to requests to /info domain
-        // with a description of the request
-        get("info") { req in
-            return req.description
-        }
-
-        get("description") { req in return req.description }
-        
-        try resource("posts", PostController.self)
+  func setupRoutes() throws {
+    get("hello") { req in
+      var json = JSON()
+      try json.set("hello", "world")
+      return json
     }
+    
+    get("plaintext") { req in
+      return "Hello, world!"
+    }
+    
+    get("/") { req in
+      var json = JSON()
+      try json.set("name","oye!")
+      return json
+    }
+    
+    // response to requests to /info domain
+    // with a description of the request
+    get("info") { req in
+      return req.description
+    }
+    
+    get("description") { req in return req.description }
+    
+    try resource("posts", PostController.self)
+  }
 }
+
